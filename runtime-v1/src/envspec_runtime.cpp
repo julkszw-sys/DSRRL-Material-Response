54e4f46d3d62021f7c6861f704406153b97a6417
julkszw-sys <300493217+julkszw-sys@users.noreply.github.com>
2026-09-25 01:07:31 +0200

    Runtime v1: fix EnvSpec preflight Windows minmax

--- runtime-v1/src/envspec_runtime.cpp
+++ runtime-v1/src/envspec_runtime.cpp
@@ -1,3 +1,10 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
 #include "dsrrl/runtime/envspec_runtime.hpp"
 
 #include "dsrrl/operators/env_spec/legacy_resource_bridge.hpp"
