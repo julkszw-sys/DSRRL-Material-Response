674bb47e348a5050e224ad8d7471a48b128ca701
julkszw-sys <300493217+julkszw-sys@users.noreply.github.com>
2026-09-24 22:11:12 +0200

    Build151: catalog owner-proven P_Metal V10 operator

--- renderer-core/src/operator_catalog.cpp
+++ renderer-core/src/operator_catalog.cpp
@@ -30,6 +30,7 @@ constexpr std::array<operator_contract, operator_count> k_catalog = {{
     {operator_id::dsr_native_sfx, "dsr_native_sfx_island", "renderer.bridge.dsr_native_sfx_island_multigate_boundary_v1", "DSR Native Spell / VFX Island", canonical_status::high_confidence, port_state::stock_host, carrier_kind::host_preserve, require_none, true},
     {operator_id::dsr_sfx_inverse_tonemap, "dsr.sfxpbl.inverse_tonemap", "renderer.dsr.sfxpbl.dedicated_inverse_tonemap_operator_v1", "DSR dedicated SfxPBL inverse-tonemap", canonical_status::confirmed, port_state::stock_host, carrier_kind::host_preserve, require_none, true},
     {operator_id::pmetal_black_safe_source, "surface.pmetal_black_safe_source", "project.materialization.pmetal_envspec_v13_ptde_donor_ab_v1", "P_Metal V13 black-safe source", canonical_status::confirmed, port_state::off, carrier_kind::hybrid, require_receiver | require_material | require_resource | require_producer | require_consumer, true},
+    {operator_id::pmetal_black_safe_v10, "surface.pmetal_black_safe_v10", "project.runtime.pmetal_blackspot_v10_paired_lerp_fix_v1", "P_Metal V10 black-safe paired Lerp", canonical_status::confirmed, port_state::active_candidate, carrier_kind::shader, require_receiver | require_material | require_consumer, true},
 }};
 static_assert(k_catalog.size() == operator_count, "Every operator_id must have one catalog contract.");
 
