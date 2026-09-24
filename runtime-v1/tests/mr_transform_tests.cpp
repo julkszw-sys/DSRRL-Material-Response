bae8591a1ddf863fd481a3faf166954e178009ec
julkszw-sys <300493217+julkszw-sys@users.noreply.github.com>
2026-09-24 18:47:18 +0200

    Renderer Core A2 SpecRGB: lock exact per-host size growth regression

--- runtime-v1/tests/mr_transform_tests.cpp
+++ runtime-v1/tests/mr_transform_tests.cpp
@@ -8,6 +8,10 @@ using namespace dsrrl::runtime::mr;
 
 int main()
 {
+    constexpr std::array<std::uint32_t,24> k_spec_growth = {{
+        1104u,1104u,1040u,1040u,1040u,976u,912u,912u,848u,848u,848u,784u,
+        976u,976u,912u,912u,912u,848u,848u,848u,784u,784u,784u,720u
+    }};
     CHECK(k_plans.size()==24u);
     for(std::size_t i=0;i<k_plans.size();++i){
         CHECK(k_plans[i].index==i);
@@ -21,7 +25,7 @@ int main()
         CHECK(k_plans[i].spec_dcl_t1_word<k_plans[i].spec_sample_t1_word);
         CHECK(k_plans[i].v29_specrgb_sha256.size()==64u);
         CHECK(k_plans[i].v211_specrgb_sha256.size()==64u);
-        CHECK(k_plans[i].specrgb_replacement_size==k_plans[i].replacement_size+1104u);
+        CHECK(k_plans[i].specrgb_replacement_size==k_plans[i].replacement_size+k_spec_growth[i]);
         CHECK(find_plan(k_plans[i].stock_size,k_plans[i].original_sha256)==&k_plans[i]);
     }
     CHECK(find_plan(0,"nope")==nullptr);
