c68b64cdfddb17b7b5f253c3e98cad3a9c4559ea
julkszw-sys <300493217+julkszw-sys@users.noreply.github.com>
2026-09-25 20:12:18 +0200

    HemDir3: replace branch-copy detour with relocation-safe selector cleanup

--- renderer-core/integrated/hemdir3_mode_detour.asm
+++ renderer-core/integrated/hemdir3_mode_detour.asm
@@ -1,11 +1,13 @@
 option casemap:none
 
 EXTERN dsrrl_hemdir3_effective_mode_observer:PROC
+EXTERN dsrrl_hemdir3_selector_end_observer:PROC
 EXTERN g_dsrrl_hemdir3_mode_lt5_trampoline:QWORD
-EXTERN g_dsrrl_hemdir3_mode_ge5_trampoline:QWORD
+EXTERN g_dsrrl_hemdir3_selector_end_trampoline:QWORD
 
 .code
 
+; Inside 0x140295F50 the stack is still the callee-entry stack (8 mod 16).
 PUBLIC dsrrl_hemdir3_mode_lt5_hook_entry
 dsrrl_hemdir3_mode_lt5_hook_entry PROC
     pushfq
@@ -47,8 +49,9 @@ dsrrl_hemdir3_mode_lt5_hook_entry PROC
     jmp qword ptr [g_dsrrl_hemdir3_mode_lt5_trampoline]
 dsrrl_hemdir3_mode_lt5_hook_entry ENDP
 
-PUBLIC dsrrl_hemdir3_mode_ge5_hook_entry
-dsrrl_hemdir3_mode_ge5_hook_entry PROC
+; At 0x14022BA79 the parent selector has a 16-byte aligned local stack.
+PUBLIC dsrrl_hemdir3_selector_end_hook_entry
+dsrrl_hemdir3_selector_end_hook_entry PROC
     pushfq
     push rax
     push rcx
@@ -57,26 +60,25 @@ dsrrl_hemdir3_mode_ge5_hook_entry PROC
     push r9
     push r10
     push r11
-    sub rsp,0A8h
+    sub rsp,0A0h
 
-    movdqu xmmword ptr [rsp+30h],xmm0
-    movdqu xmmword ptr [rsp+40h],xmm1
-    movdqu xmmword ptr [rsp+50h],xmm2
-    movdqu xmmword ptr [rsp+60h],xmm3
-    movdqu xmmword ptr [rsp+70h],xmm4
-    movdqu xmmword ptr [rsp+80h],xmm5
+    movdqu xmmword ptr [rsp+20h],xmm0
+    movdqu xmmword ptr [rsp+30h],xmm1
+    movdqu xmmword ptr [rsp+40h],xmm2
+    movdqu xmmword ptr [rsp+50h],xmm3
+    movdqu xmmword ptr [rsp+60h],xmm4
+    movdqu xmmword ptr [rsp+70h],xmm5
 
-    mov ecx,edx
-    call dsrrl_hemdir3_effective_mode_observer
+    call dsrrl_hemdir3_selector_end_observer
 
-    movdqu xmm0,xmmword ptr [rsp+30h]
-    movdqu xmm1,xmmword ptr [rsp+40h]
-    movdqu xmm2,xmmword ptr [rsp+50h]
-    movdqu xmm3,xmmword ptr [rsp+60h]
-    movdqu xmm4,xmmword ptr [rsp+70h]
-    movdqu xmm5,xmmword ptr [rsp+80h]
+    movdqu xmm0,xmmword ptr [rsp+20h]
+    movdqu xmm1,xmmword ptr [rsp+30h]
+    movdqu xmm2,xmmword ptr [rsp+40h]
+    movdqu xmm3,xmmword ptr [rsp+50h]
+    movdqu xmm4,xmmword ptr [rsp+60h]
+    movdqu xmm5,xmmword ptr [rsp+70h]
 
-    add rsp,0A8h
+    add rsp,0A0h
     pop r11
     pop r10
     pop r9
@@ -85,7 +87,7 @@ dsrrl_hemdir3_mode_ge5_hook_entry PROC
     pop rcx
     pop rax
     popfq
-    jmp qword ptr [g_dsrrl_hemdir3_mode_ge5_trampoline]
-dsrrl_hemdir3_mode_ge5_hook_entry ENDP
+    jmp qword ptr [g_dsrrl_hemdir3_selector_end_trampoline]
+dsrrl_hemdir3_selector_end_hook_entry ENDP
 
 END
