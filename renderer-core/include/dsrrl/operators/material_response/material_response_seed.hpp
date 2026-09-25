563f5bb3a631968ed1d6bc4f6126206da587e194
julkszw-sys <300493217+julkszw-sys@users.noreply.github.com>
2026-09-25 18:13:20 +0200

    Material Response: expose confirmed stable receiver seed

--- renderer-core/include/dsrrl/operators/material_response/material_response_seed.hpp
+++ renderer-core/include/dsrrl/operators/material_response/material_response_seed.hpp
@@ -7,5 +7,6 @@
 namespace dsrrl::operators::material_response {
 
 std::size_t register_confirmed_material_routes_v1(material_response_island &island);
+std::size_t register_confirmed_material_receivers_v1(material_response_island &island);
 
 } // namespace dsrrl::operators::material_response
