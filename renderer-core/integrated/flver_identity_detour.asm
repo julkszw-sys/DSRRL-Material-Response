5fd7f053203e086c33d18dc8872f4d8a204ea729
julkszw-sys <300493217+julkszw-sys@users.noreply.github.com>
2026-09-25 20:01:32 +0200

    Core selector: preserve full owner assignment ABI for UpperLower freshness

--- renderer-core/integrated/flver_identity_detour.asm
+++ renderer-core/integrated/flver_identity_detour.asm
@@ -1,43 +1,63 @@
 option casemap:none
+
 EXTERN dsrrl_flver_selector_observer:PROC
 EXTERN g_dsrrl_flver_selector_trampoline:QWORD
+
 .code
+
 PUBLIC dsrrl_flver_selector_hook_entry
 dsrrl_flver_selector_hook_entry PROC
- pushfq
- push rax
- push rcx
- push rdx
- push r8
- push r9
- push r10
- push r11
- sub rsp,0A8h
- movdqu xmmword ptr [rsp+30h],xmm0
- movdqu xmmword ptr [rsp+40h],xmm1
- movdqu xmmword ptr [rsp+50h],xmm2
- movdqu xmmword ptr [rsp+60h],xmm3
- movdqu xmmword ptr [rsp+70h],xmm4
- movdqu xmmword ptr [rsp+80h],xmm5
- mov rcx,qword ptr [rsp+0D0h]
- mov rdx,qword ptr [rsp+0C0h]
- mov r8,qword ptr [rsp+0E8h]
- call dsrrl_flver_selector_observer
- movdqu xmm0,xmmword ptr [rsp+30h]
- movdqu xmm1,xmmword ptr [rsp+40h]
- movdqu xmm2,xmmword ptr [rsp+50h]
- movdqu xmm3,xmmword ptr [rsp+60h]
- movdqu xmm4,xmmword ptr [rsp+70h]
- movdqu xmm5,xmmword ptr [rsp+80h]
- add rsp,0A8h
- pop r11
- pop r10
- pop r9
- pop r8
- pop rdx
- pop rcx
- pop rax
- popfq
- jmp qword ptr [g_dsrrl_flver_selector_trampoline]
+    pushfq
+    push rax
+    push rcx
+    push rdx
+    push r8
+    push r9
+    push r10
+    push r11
+    sub rsp,0A8h
+
+    ; 20h..2Fh are argument 5/6 spill locations after the required shadow
+    ; space. Original S0 is rsp+0E8h after eight pushes and sub 0A8h.
+    movdqu xmmword ptr [rsp+30h],xmm0
+    movdqu xmmword ptr [rsp+40h],xmm1
+    movdqu xmmword ptr [rsp+50h],xmm2
+    movdqu xmmword ptr [rsp+60h],xmm3
+    movdqu xmmword ptr [rsp+70h],xmm4
+    movdqu xmmword ptr [rsp+80h],xmm5
+
+    ; Exact retail selector ABI:
+    ; original RCX = material container
+    ; original RDX = LightBank/render owner
+    ; original R8D = material index
+    ; return address identifies selector caller variant
+    ; live R14/R15 carry the assignment descriptor depending on caller.
+    mov rcx,qword ptr [rsp+0D0h]
+    mov rdx,qword ptr [rsp+0C8h]
+    mov r8,qword ptr [rsp+0E8h]
+    mov r9,r14
+    mov qword ptr [rsp+20h],r15
+    mov rax,qword ptr [rsp+0C0h]
+    mov qword ptr [rsp+28h],rax
+    call dsrrl_flver_selector_observer
+
+    movdqu xmm0,xmmword ptr [rsp+30h]
+    movdqu xmm1,xmmword ptr [rsp+40h]
+    movdqu xmm2,xmmword ptr [rsp+50h]
+    movdqu xmm3,xmmword ptr [rsp+60h]
+    movdqu xmm4,xmmword ptr [rsp+70h]
+    movdqu xmm5,xmmword ptr [rsp+80h]
+
+    add rsp,0A8h
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdx
+    pop rcx
+    pop rax
+    popfq
+    jmp qword ptr [g_dsrrl_flver_selector_trampoline]
 dsrrl_flver_selector_hook_entry ENDP
+
 END
