4c494b792cf917e4d2d8e9e13edd4fcec18e6e8a
julkszw-sys <300493217+julkszw-sys@users.noreply.github.com>
2026-09-26 15:06:15 +0200

    UpperLower: add exact Phn PntS receiver family

--- renderer-core/include/dsrrl/operators/lightbank/upper_lower_hemenv_materializer.hpp
+++ renderer-core/include/dsrrl/operators/lightbank/upper_lower_hemenv_materializer.hpp
@@ -30,7 +30,8 @@ enum class upper_lower_hemenv_family : std::uint8_t {
     hemenv = 0,
     hemenvlerp,
     hemenv_parallax,
-    hemenvlerp_parallax
+    hemenvlerp_parallax,
+    phn_pnts
 };
 
 struct upper_lower_hemenv_materialize_outcome {
